a237ec7d7eb7349aef3eab8cfa9e3cdbda2d8a34
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-07-21 00:41:12 +0300

    c

--- js/common.js
+++ js/common.js
@@ -1,44 +1,57 @@
-let synergyMenu = function () {
-	let menu = document.querySelector('.menu');
-	let content = document.querySelector('.main-content');
-	let menuBtn = document.querySelector('#btnCloseMenu');
-	let menuLinks = document.querySelectorAll('.menu .menu__link');
+let siteLogic = {
+	// properties
+	
+	content: document.querySelector('.main-content'),
+	menu: document.querySelector('.menu'),
+	menuBtn: document.querySelector('#btnCloseMenu'),
+	menuLinks: document.querySelectorAll('.menu .menu__link'),
+	// defaults
+	isMobWidth: false,
 
+	// main
+	init () {
+		this.fixedWidth();
+		this.widthListener();
+		this.menuListener()
+	},
 
-	let fixedWidth = function () {
-		if (menu.classList.contains('active')) {
-			content.classList.remove('menu-active')
+	// methods
+
+	fixedWidth() {
+		if (this.menu.classList.contains('active')) {
+			this.content.classList.remove('menu-active')
 		} else {
-			content.classList.add('menu-active')
+			this.content.classList.add('menu-active')
 		}
-	}
-	let arrRemoveClass = function (arr) {
+	},
+
+	arrRemoveClass(arr) {
 		arr.forEach(item => {
 			item.classList.remove('active');
 		})
-	}
-	fixedWidth()
+	},
 
-	menuBtn.addEventListener('click', function (e) {
-		menu.classList.toggle('active');
-		fixedWidth()
-	})
-
-	window.addEventListener('resize', function (e) {
-		if (window.innerWidth < 768) {
-			menu.classList.add('active')
-		}
-		else {
-			menu.classList.remove('active')
-		}
-	})
-
-	menuLinks.forEach(link => {
-		link.addEventListener('click', function () {
-			arrRemoveClass(menuLinks);
-			this.classList.add('active')
+	// listeners
+	widthListener() {
+		let self = this;
+		window.addEventListener('resize', function (e) {
+			if (window.innerWidth < 768) {
+				self.menu.classList.add('active');
+				self.content.classList.remove('menu-active');
+				self.isMobWidth = true;
+			}
+			else {
+				self.menu.classList.remove('active');
+				self.content.classList.add('menu-active');
+				self.isMobWidth = false;
+			}
 		})
-	})
+	},
+	menuListener() {
+		this.menuBtn.addEventListener('click', () => {
+			this.menu.classList.toggle('active')
+		})
+	}
 }
 
-synergyMenu()
+siteLogic.init()
